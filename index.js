a1fdb6a8aecb83e4b591613bcd15c837dae8a1d6
kuberdhure <kuberdhureofficial@gmail.com>
2023-07-07 21:32:08 +0530

    Add color functionality

--- index.js
+++ index.js
@@ -1,52 +1,66 @@
 let doc = document;
-let container = doc.querySelector('.container');
-let slider = doc.querySelector('.grid-size-val');
-let gridSizeLabel = doc.querySelector('.grid-size-label')
-let resetBtn = doc.querySelector('.reset')
+let container = doc.querySelector(".container");
+let slider = doc.querySelector(".grid-size-val");
+let gridSizeLabel = doc.querySelector(".grid-size-label");
+let resetBtn = doc.querySelector(".reset");
 let gridSize = 2;
+let colBtnBlk = doc.querySelector(".col-black");
+let colBtnRGB = doc.querySelector(".col-rgb");
+let cellColor = 'black';
+
 slider.min = 2;
 slider.max = 64;
 
-
 doc.body.appendChild(container);
 
-function createCells(gridSize){
-    
-    // Clear existing cells
-    container.innerHTML = '';
-    gridSizeLabel.innerText = `Grid size: ${gridSize} x ${gridSize}`
-    const cellSize = `calc(100% / ${gridSize})`;
+function createCells(gridSize) {
+  // Clear existing cells
+  container.innerHTML = "";
+  gridSizeLabel.innerText = `Grid size: ${gridSize} x ${gridSize}`;
+  const cellSize = `calc(100% / ${gridSize})`;
 
-    for (let i = 0; i < Math.pow(gridSize, 2); i++) {
-        let pixel = doc.createElement('div');
-      pixel.addEventListener('mouseover', function (event) {
-        event.target.style.backgroundColor = 'black';
-      });
-      pixel.style.width = cellSize;
-      pixel.style.height = cellSize;
-      container.appendChild(pixel);
-      pixel.className = "pixel";
-    }
-    
+  for (let i = 0; i < Math.pow(gridSize, 2); i++) {
+    let pixel = doc.createElement("div");
+    pixel.addEventListener("mouseover", function (event) {
+      event.target.style.backgroundColor = cellColor == 'black'? 'black' : getRandomRGBColor();
+    });
+    pixel.style.width = cellSize;
+    pixel.style.height = cellSize;
+    container.appendChild(pixel);
+    pixel.className = "pixel";
+  }
 }
 
-
-function resetCells(){
-  container.innerHTML = ''; 
-  createCells(slider.value)
+function resetCells() {
+  container.innerHTML = "";
+  createCells(slider.value);
 }
 
+function getRandomRGBColor() {
+  var red = Math.floor(Math.random() * 128) + 128;
+  var green = Math.floor(Math.random() * 128) + 128;
+  var blue = Math.floor(Math.random() * 128) + 128;
 
+  var rgbColor = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
+  return rgbColor;
+}
 
 createCells(gridSize);
 
-
-slider.addEventListener('input',(event)=>{
-    gridSize = event.target.value;
-    createCells(gridSize);
-    console.log(gridSize)
+slider.addEventListener("input", (event) => {
+  gridSize = event.target.value;
+  createCells(gridSize);
+  console.log(gridSize);
 });
 
-resetBtn.addEventListener('click',()=>{
-  resetCells()
+resetBtn.addEventListener("click", () => {
+  resetCells();
+});
+
+colBtnRGB.addEventListener("click",() => {
+    cellColor = 'rgb';
+});
+
+colBtnBlk.addEventListener("click",() => {
+  cellColor = 'black';
 });
